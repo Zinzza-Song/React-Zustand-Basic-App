be605837f003d838229378863ba2da27102c3ea8
Han-Bin Song <thdgksq@naver.com>
2025-03-29 14:41:27 +0900

    persist 미들웨어를 활용하여 로컬 스토리지와 세션 스토리지 활용 기능 구현

--- src/components/Coutner.jsx
+++ src/components/Coutner.jsx
@@ -4,6 +4,10 @@ import { useCounterStore } from "../store/useCounterStore";
 const Coutner = () => {
   const { count, increment, reset, setNumber } = useCounterStore();
 
+  const clear = () => {
+    useCounterStore.persist.clearStorage();
+  };
+
   return (
     <div>
       <p style={{ color: "white", fontWeight: "bold" }}>{count}</p>
@@ -18,6 +22,10 @@ const Coutner = () => {
 
       <button style={{ marginRight: "10px" }} onClick={() => setNumber(3)}>
         to 3
+      </button>
+
+      <button style={{ marginRight: "10px" }} onClick={clear}>
+        clear
       </button>
     </div>
   );
