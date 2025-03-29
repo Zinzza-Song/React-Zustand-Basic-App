ee46d9847cd24d40b5a4c8ae055df9c8aa029284
Han-Bin Song <thdgksq@naver.com>
2025-03-29 13:49:03 +0900

    기능 추가

--- src/components/Coutner.jsx
+++ src/components/Coutner.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import { useCounterStore } from "../store";
 
 const Coutner = () => {
-  const { count, increment } = useCounterStore();
+  const { count, increment, reset, setNumber } = useCounterStore();
 
   return (
     <div>
       <p style={{ color: "white", fontWeight: "bold" }}>{count}</p>
+
       <button onClick={increment}>+1</button>
+      <br />
+
+      <button style={{ marginTop: "10px" }} onClick={reset}>
+        reset
+      </button>
+      <br />
+
+      <button style={{ marginTop: "10px" }} onClick={() => setNumber(3)}>
+        to 3
+      </button>
     </div>
   );
 };
